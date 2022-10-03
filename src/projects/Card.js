dad7479d2003f1843e4cb31ab2eff54dc95a0a00
joycethh <joycethh@outlook.com>
2022-10-03 13:49:01 -0500

    create Card example

--- src/projects/Card.js
+++ src/projects/Card.js
@@ -2,7 +2,8 @@ import React from "react";
 import {
   Card,
   CardMedia,
-  CardActions,
+  Button,
+  Stack,
   CardContent,
   Typography,
   Chip,
@@ -11,51 +12,69 @@ import {
 import memory from "../images/memory.png";
 import workout from "../images/workout.png";
 
-const MediaOverlay = styled("div")(({ theme }) => ({
+const OverlayBox = styled("div")(({ theme }) => ({
+  color: "#ffffff",
   position: "absolute",
   top: 0,
-  right: 0,
   bottom: 0,
   left: 0,
-  boxSizing: "border-box",
-  background: theme.palette.secondary.light,
-  color: "#fff",
-  transition: "all 0.4s ease-in-out",
+  right: 0,
+  height: "100%",
+  width: "100%",
+  opacity: 0,
+  backgroundColor: " rgba(37,42,52,1)",
+  transition: "0.5s ease-in-out",
+  "&:hover": {
+    opacity: 1,
+  },
 }));
+
+const StyledImg = styled(CardMedia)({
+  display: "block",
+  maxWidth: "100%",
+  opacity: 1,
+  transition: ".5s ease",
+  backfaceVisibility: "hidden",
+  "&:hover": {
+    opacity: 0.1,
+  },
+});
+
+const TextBox = styled("div")({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+});
 
 const MyCard = () => {
   return (
     <>
-      {/* <Card
-        sx={{
-          maxWidth: 345,
-          alignItem: "center",
-          textAlign: "center",
-        }}
-      >
-        <CardMedia component="img" image={memory} alt="react" />
-        <MediaOverlay component="div">
-          <Typography variant="h6" sx={{ width: "50%" }}>
-            memory
-          </Typography>
-        </MediaOverlay>
+      <Card sx={{ position: "relative", width: "50%" }}>
+        <StyledImg component="img" image={workout} alt="workout" />
         <CardContent>
           <Chip label="React" />
           <Chip label="MongoDB" />
           <Chip label="Express" />
           <Chip label="Node JS" />
         </CardContent>
-      </Card> */}
-      <div className="container">
-        <img className="img" src={memory} alt="react" />
-        <div className="overlay">
-          <div className="text">
-            <h4>social media for user to create, share, search posts. </h4>
-            <button>Visit Website </button>
-            <button>View Codes</button>
-          </div>
-        </div>
-      </div>
+        <OverlayBox component="div">
+          <TextBox>
+            <Typography variant="h6" pb={2}>
+              Social media web app using CRUD mechanism that allows users to
+              create, update and delete posts.
+            </Typography>
+            <Stack direction="row" spacing={2}>
+              <Button variant="outlined" size="small" color="secondary">
+                Visit Website
+              </Button>
+              <Button variant="outlined" size="small" color="secondary">
+                View Codes
+              </Button>
+            </Stack>
+          </TextBox>
+        </OverlayBox>
+      </Card>
     </>
   );
 };
