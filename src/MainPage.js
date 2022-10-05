7653b84335622269ce147196d1d9e47ae242edac
joycethh <joycethh@outlook.com>
2022-10-05 17:05:39 -0500

    wrap the app with browserRouter

---
+++ src/MainPage.js
@@ -0,0 +1,17 @@
+import Home from "./home/Home";
+import Skill from "./skills/Skill";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
+
+const MainPage = () => {
+  return (
+    <>
+      <Home />
+      <Skill />
+      <Projects />
+      <Contact />
+    </>
+  );
+};
+
+export default MainPage;
