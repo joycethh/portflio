a544fc3168ca5eedd01cab34029f66ac0d0b8910
joycethh <joycethh@outlook.com>
2022-10-05 13:50:05 -0500

    complete contact section

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -2,22 +2,18 @@ import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react";
 import {
   Box,
-  Grid,
-  Button,
   Typography,
   Card,
   CardContent,
-  Stack,
-  IconButton,
   Dialog,
   DialogTitle,
   DialogContent,
   DialogContentText,
 } from "@mui/material";
 
-import { Send, GitHub, LinkedIn, Email } from "@mui/icons-material/";
 import SectionContainer from "../SectionContainer";
-import Input from "./Input";
+import Form from "./Form";
+import Socials from "../Socials";
 
 const Contact = () => {
   const [error, setError] = useState(null);
@@ -58,59 +54,32 @@ const Contact = () => {
           }}
         >
           <Card>
-            <CardContent>
+            <CardContent sx={{ padding: 4 }}>
               <Typography variant="body1" gutterBottom>
-                I am open with new opporturnities. If you are interested in
-                hiring me for your projects, use the form below to send me a
-                message. Let's make something nice!
+                I am open with new opporturnities. Send me a message and let's
+                make something nice!
               </Typography>
             </CardContent>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
+
+            <CardContent
+              sx={{
+                paddingLeft: 4,
+                paddingRight: 4,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h6">
                 You can also find me on the following channels
               </Typography>
             </CardContent>
-            <Stack direction="row" spacing={2}>
-              <IconButton href="https://github.com/joycethh" target="_blank">
-                <GitHub />
-              </IconButton>
-              <IconButton
-                href="https://www.linkedin.com/in/joycethh/"
-                target="_blank"
-              >
-                <LinkedIn />
-              </IconButton>
-              <IconButton href="mailto: joycethhh@gmail.com" target="_blank">
-                <Email />
-              </IconButton>
-            </Stack>
+            <Socials />
           </Card>
 
           <form ref={refForm} onSubmit={sendEmail}>
-            <Box pt={5}>
-              <Grid container spacing={3}>
-                <Input name="name" label="Name" half />
-                <Input name="email" label="Email" type="email" half />
-                <Input name="message" label="Message" multiline rows={7} />
-
-                <Grid
-                  item
-                  xs={12}
-                  sx={{ display: "flex", justifyContent: "center" }}
-                >
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    type="submit"
-                    endIcon={<Send />}
-                    sx={{ width: "100%" }}
-                  >
-                    SEND
-                  </Button>
-                </Grid>
-              </Grid>
-            </Box>
+            <Form />
           </form>
+
           {sucess && (
             <Dialog open={open} onClose={() => setOpen(false)}>
               <DialogTitle> Your message is sent!</DialogTitle>
