f899a39e700b2ec69d6fa39293bba90b7b408ed0
joycethh <joycethh@outlook.com>
2022-10-07 11:35:27 -0500

    move the skills overview section to skills

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { Container, Grid, Typography, Paper, Box, Fab } from "@mui/material";
-import {
-  ImportantDevices,
-  ArrowForward,
-  Code,
-  TipsAndUpdates,
-} from "@mui/icons-material";
+import { Container, Grid, Typography } from "@mui/material";
+import { ArrowForward } from "@mui/icons-material";
 import { Link } from "react-scroll";
 
 import { AvatarBox, BioBox, StyledButton } from "./Elements";
@@ -16,117 +11,46 @@ const Home = () => {
   return (
     <div id="home">
       <Container>
-        <Box>
-          <Grid container>
-            <Grid item xs={12} md={3}>
-              <AvatarBox>
-                <Avatar square={true} />
-              </AvatarBox>
-            </Grid>
-
-            <Grid item xs={12} md={9}>
-              <BioBox mt={2}>
-                <Typography variant="body1">Hello, my name is</Typography>
-              </BioBox>
-              <BioBox mt={1}>
-                <Typography variant="h3">Joyce Tang</Typography>
-              </BioBox>
-              <BioBox mt={2}>
-                <Typography variant="body1">
-                  I am a self-started
-                  <strong> front-end web developer</strong> focusing on creating
-                  <strong> beautiful </strong> web applications while wrting
-                  <strong> clean and maintainable </strong> codes.
-                </Typography>
-              </BioBox>
-              <BioBox mt={5}>
-                <Link
-                  to="projects"
-                  spy={true}
-                  smooth={true}
-                  offset={0}
-                  duration={500}
-                >
-                  <StyledButton variant="outlined" endIcon={<ArrowForward />}>
-                    View Projects
-                  </StyledButton>
-                </Link>
-              </BioBox>
-            </Grid>
+        <Grid container>
+          <Grid item xs={12} md={3}>
+            <AvatarBox>
+              <Avatar square={true} />
+            </AvatarBox>
           </Grid>
 
-          {/* features   */}
-          <Box pt={6}>
-            <Typography variant="h5" sx={{ textAlign: "center" }}>
-              Skills Overview
-            </Typography>
-          </Box>
-          <Box pt={12}>
-            <Grid container columnSpacing={2} rowSpacing={{ sm: 4, md: 1 }}>
-              {lists.map((item) => (
-                <Grid
-                  item
-                  sm={12}
-                  md={4}
-                  key={item.title}
-                  sx={{ display: "flex", justifyContent: "center" }}
-                >
-                  <Paper
-                    elevation={2}
-                    sx={{
-                      alignItem: "center",
-                      textAlign: "center",
-                      padding: 3,
-                      width: 350,
-                      height: 220,
-                    }}
-                  >
-                    <Fab
-                      sx={{
-                        backgroundColor: "#00ADB5",
-
-                        width: "80px",
-                        height: "80px",
-                        top: "-40px",
-                      }}
-                    >
-                      {
-                        <item.icon
-                          style={{ fontSize: "40px", color: "#FAF0E6" }}
-                        />
-                      }
-                    </Fab>
-
-                    <Typography gutterBottom variant="h6">
-                      {item.title}
-                    </Typography>
-                    <Typography variant="body2">{item.body}</Typography>
-                  </Paper>
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-        </Box>
+          <Grid item xs={12} md={9}>
+            <BioBox mt={2}>
+              <Typography variant="body1">Hello, my name is</Typography>
+            </BioBox>
+            <BioBox mt={1}>
+              <Typography variant="h3">Joyce Tang</Typography>
+            </BioBox>
+            <BioBox mt={2}>
+              <Typography variant="body1">
+                I am a self-started
+                <strong> front-end web developer</strong> focusing on creating
+                <strong> beautiful </strong> web applications while wrting
+                <strong> clean and maintainable </strong> codes.
+              </Typography>
+            </BioBox>
+            <BioBox mt={5}>
+              <Link
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={500}
+              >
+                <StyledButton variant="outlined" endIcon={<ArrowForward />}>
+                  View Projects
+                </StyledButton>
+              </Link>
+            </BioBox>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
 };
 
-const lists = [
-  {
-    icon: Code,
-    title: "Web Development",
-    body: "I am always aimed for responsive layouts communicative and creative designs and fast-loading web applications. ",
-  },
-  {
-    icon: ImportantDevices,
-    title: "Web Design",
-    body: "A focus placed on the communication, interation and probelm-solving through thoughtful web designs.",
-  },
-  {
-    icon: TipsAndUpdates,
-    title: "Ability of Learning",
-    body: "It is really important to keep in trend with rapid developing technologies. Strong preference on using the newest and reliable techonologies",
-  },
-];
 export default Home;
--- src/skills/Skill.js
+++ src/skills/Skill.js
@@ -6,11 +6,14 @@ import {
   Grid,
   Typography,
   styled,
+  Paper,
+  Box,
+  Fab,
 } from "@mui/material";
-
+import { ImportantDevices, Code, TipsAndUpdates } from "@mui/icons-material";
 import SectionContainer from "../SectionContainer";
 
-const Box = styled("div")(({ theme }) => ({
+const TitleBox = styled("div")(({ theme }) => ({
   padding: theme.spacing(6),
   display: "flex",
   justifyContent: "center",
@@ -21,51 +24,123 @@ const Skill = () => {
   return (
     <div id="skills">
       <SectionContainer title="Skills">
-        <Box component="div">
-          <Typography variant="h6" sx={{ textAlign: "center" }}>
-            My Top Skills
-          </Typography>
-        </Box>
-        <Box component="div">
-          <Grid container>
-            {front.map((item) => (
-              <Grid item xs={6} sm={3} md={2} key={item.title}>
-                <Card
-                  elevation={0}
+        {/* overview   */}
+        <TitleBox component="div">
+          <Typography variant="h5">Skills Overview</Typography>
+        </TitleBox>
+
+        <Grid
+          container
+          spacing={{ xs: 5, md: 2 }}
+          sx={{
+            backgroundColor: "coral",
+            display: "flex",
+            justifyContent: "center",
+            pt: 12,
+            pb: 3,
+          }}
+        >
+          {lists.map((item) => (
+            <Grid
+              item
+              sm={12}
+              md={4}
+              key={item.title}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Paper
+                elevation={2}
+                sx={{
+                  alignItem: "center",
+                  textAlign: "center",
+                  padding: 3,
+                  width: { xs: 350, md: 300, lg: 350 },
+                  height: { xs: 220, md: 270, lg: 220 },
+                }}
+              >
+                <Fab
                   sx={{
-                    alignItem: "center",
-                    textAlign: "center",
-                    paddingTop: "20px",
-                    width: {
-                      sx: 50,
-                      sm: 80,
-                      md: 120,
-                    },
+                    backgroundColor: "#00ADB5",
+                    width: "80px",
+                    height: "80px",
+                    top: "-50px",
                   }}
                 >
-                  <CardMedia
-                    component="img"
-                    image={item.src}
-                    alt={item.title}
-                    sx={{
-                      width: { xs: "35px", md: "60px", xl: "80px" },
-                      margin: "auto",
-                    }}
-                  />
-                  <CardContent>
-                    <Typography variant="body1" component="div" gutterBottom>
-                      {item.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
+                  {<item.icon style={{ fontSize: "40px", color: "#FAF0E6" }} />}
+                </Fab>
+
+                <Typography gutterBottom variant="h6">
+                  {item.title}
+                </Typography>
+                <Typography variant="body2">{item.body}</Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+
+        {/* lists */}
+        <TitleBox component="div">
+          <Typography variant="h5">My Top Skills</Typography>
+        </TitleBox>
+        <Grid container>
+          {front.map((item) => (
+            <Grid item xs={6} sm={3} md={2} key={item.title}>
+              <Card
+                elevation={0}
+                sx={{
+                  alignItem: "center",
+                  textAlign: "center",
+                  paddingTop: "20px",
+                  width: {
+                    sx: 50,
+                    sm: 80,
+                    md: 120,
+                  },
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  image={item.src}
+                  alt={item.title}
+                  sx={{
+                    width: { xs: "35px", md: "60px", xl: "80px" },
+                    margin: "auto",
+                  }}
+                />
+                <CardContent>
+                  <Typography variant="body1" component="div" gutterBottom>
+                    {item.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </SectionContainer>
     </div>
   );
 };
+
+const lists = [
+  {
+    icon: Code,
+    title: "Web Development",
+    body: "I am always aimed for responsive layouts communicative and creative designs and fast-loading web applications. ",
+  },
+  {
+    icon: ImportantDevices,
+    title: "Web Design",
+    body: "A focus placed on the communication, interation and probelm-solving through thoughtful web designs.",
+  },
+  {
+    icon: TipsAndUpdates,
+    title: "Ability of Learning",
+    body: "It is really important to keep in trend with rapid developing technologies. Strong preference on using the newest and reliable techonologies",
+  },
+];
 
 const front = [
   {
